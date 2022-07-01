1e7fcd703a5c27b6f088a3f6a577aed0a31b7987
Bhavya Verma <vbhavya269@gmail.com>
2022-07-01 23:12:51 +0530

    ✨ Event handling using EventEmitter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// Common js import syntax
-const Person = require('./person');
+const Logger = require('./logger');
 
-const person1 = new Person('John', 30);
+const logger = new Logger();
 
-person1.greeting();  
+logger.on('message', (data) => console.log(`Called Listener:`, data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
