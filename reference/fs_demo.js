0e10af8a7133220bc199709cf36cf631b355db93
Bhavya Verma <vbhavya269@gmail.com>
2022-07-01 13:07:16 +0530

    ⚡️ Creating and writing to a file

---
+++ reference/fs_demo.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+// create a folder
+// fs.mkdir(path.join(__dirname, 'test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+
+// create and write to a file
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!',
+    err => {
+    if (err) throw err;
+    console.log('File written to...');
+});
+
+
