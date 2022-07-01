df8c6b667d67b54b5cb4928708800b92891c811e
Bhavya Verma <vbhavya269@gmail.com>
2022-07-01 13:43:34 +0530

    ⚡️Renaming a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -27,3 +27,10 @@ fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+
+// rename file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'),
+    (err) => {
+    if (err) throw err;
+    console.log("File renamed");
+});
