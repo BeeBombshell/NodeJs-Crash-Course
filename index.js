3fe32c4773ccf0292241d3961152cb779469fb80
Bhavya Verma <vbhavya269@gmail.com>
2022-07-02 15:15:16 +0530

    ⚡️Navigating to pages using fs module

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Home</h1>');
+
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    }
+
+    if (req.url == '/about') {
+
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
     }
 });
 
