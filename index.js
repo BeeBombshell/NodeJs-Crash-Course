699dd49e2012524d1d3a75cbbcb24d2c2b8d788e
Bhavya Verma <vbhavya269@gmail.com>
2022-07-02 17:00:02 +0530

    ⚡️ Setting filePath and contentType

--- index.js
+++ index.js
@@ -3,31 +3,65 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url == '/') {
+    
+    // Is not good practice
+    // if (req.url == '/') {
 
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    }
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' });
+    //         res.end(content);
+    //     });
+    // }
 
-    if (req.url == '/about') {
+    // if (req.url == '/about') {
 
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    }
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' });
+    //         res.end(content);
+    //     });
+    // }
 
-    if (req.url == '/api/users') {
-        const users = [
-            { name: 'Jerry Smith', age: 40 },
-            { name: 'Rick Sanchez', age: 78 },
-        ];
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
+    // if (req.url == '/api/users') {
+    //     const users = [
+    //         { name: 'Jerry Smith', age: 40 },
+    //         { name: 'Rick Sanchez', age: 78 },
+    //     ];
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    
+    // extension of file
+    let extname = path.extname(filePath);
+
+    // initial content type
+    let contentType = 'text/html';
+
+    // check extension and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        
+        case '.css':
+            contentType = 'text/css';
+            break;
+
+        case '.json':
+            contentType = 'application/json';
+            break;
+        
+        case '.png':
+            contentType = 'image/png';
+            break;
+        
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
 });
 
