f8ceceb32f1ad19a7f52debdb2490655878d99d8
Bhavya Verma <vbhavya269@gmail.com>
2022-07-02 17:32:10 +0530

    ⚡️ Reading files and added error messages

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const server = http.createServer((req, res) => {
     // }
 
     // build file path
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let filePath = path.join (__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
     
     // extension of file
     let extname = path.extname(filePath);
@@ -63,6 +63,29 @@ const server = http.createServer((req, res) => {
             contentType = 'image/jpg';
             break;
     }
+
+    // read file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            }
+            else {
+                // some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }
+        else {
+            // sucess
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    })
 });
 
 const PORT = process.env.PORT || 5000;
